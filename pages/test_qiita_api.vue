f0b3c516d70ea54a5cd6a3d52073b99f83f67e15
makoto-yoshida <makoto.yoshida@dena.com>
2018-05-23 19:20:09 +0900

    データフェッチしてループしながらデータ表示する的なやつ挑戦中

---
+++ pages/test_qiita_api.vue
@@ -0,0 +1,39 @@
+<template>
+  <section id="qiita-posts">
+  </section>
+</template>
+
+<script>
+  import TestComponent from '~/components/TestComponent.vue'
+  import AppLogo from '~/components/AppLogo.vue'
+  import RxTest from '~/components/RxTest.vue'
+  // Ajax通信ライブラリ
+  import axios from 'axios';
+
+  export default {
+    data: () => ({
+      loading: false,
+      content: "content text",
+      count: 0
+    }),
+
+    components: {
+      TestComponent,
+      AppLogo,
+      RxTest,
+    },
+
+    methods: {
+      fetch: function () {
+        this.loading = true;
+
+        axios
+          .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+          .then(response => {
+            this.content = response;
+            this.loading = false;
+          })
+      }
+    }
+  }
+</script>
