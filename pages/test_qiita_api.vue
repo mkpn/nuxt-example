f983b3f48fe23dc2c8da34a842a466aba2fc91dc
makoto-yoshida <makoto.yoshida@dena.com>
2018-05-24 17:16:01 +0900

    やっとv-forの時にkeyを設定して動かす方法がわかった・・・

--- pages/test_qiita_api.vue
+++ pages/test_qiita_api.vue
@@ -1,5 +1,17 @@
 <template>
   <section id="qiita-posts">
+    {{content}}
+    <button @click="fetch">fetchする</button>
+
+    <ul>
+      <li v-for="post in posts" v-bind:key="post.id">
+        <span class="tag" v-for="tag in post['tags']" v-bind:key="tag.index">{{ tag['name'] }}</span>
+
+        <a v-bind:href="post['url']" target="_blank" rel="noopener noreferrer">
+          <div>{{ post['title'] }}</div>
+        </a>
+      </li>
+    </ul>
   </section>
 </template>
 
@@ -14,6 +26,7 @@
     data: () => ({
       loading: false,
       content: "content text",
+      posts: [],
       count: 0
     }),
 
@@ -26,13 +39,14 @@
     methods: {
       fetch: function () {
         this.loading = true;
-
-        axios
-          .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-          .then(response => {
-            this.content = response;
+        axios.get( 'https://qiita.com/api/v2/users/junsan50/items' )
+          .then( ( res ) => {
+            this.posts = res.data;
             this.loading = false;
-          })
+          } )
+          .catch( ( res ) => {
+            console.error( res );
+          } );
       }
     }
   }
