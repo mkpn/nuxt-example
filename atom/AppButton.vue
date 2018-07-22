c2c2d64e3e2133e52126d8f01bcd0d77c0646fe9
neesan <exp.ym.lvl@gmail.com>
2018-07-22 21:53:34 +0900

    props　値渡せない 一旦コミットしよう

---
+++ atom/AppButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="fetch">{{label}}</button>
+</template>
+
+<script>
+  export default {
+    name: "AppButton.vue",
+    data() {
+      return {}
+    },
+    props: {
+      buttonLabel: String
+    }
+  }
+</script>
+
+<style scoped>
+  .app-button {
+    color: #4fc08d;
+    border-color: #4fc08d;
+    padding: 0.5em;
+    box-shadow: 0.5px 0.5px;
+  }
+
+  .app-button:hover {
+    color: #ffffff;
+    background-color: #4fc08d;
+  }
+</style>
---
+++ pages/fetch_test.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <div>
+        <button @click="fetch">fetchする</button>
+        <app-button :button-label="アイウエオ"/>
+      </div>
+      <div>{{content}}</div>
+      <div class="loading-view" v-if="loading">
+        <p>Loading...</p>
+      </div>
+    </div>
+    <div class="search-form">
+      <app-button/>
+    </div>
+    <rx-test/>
+  </section>
+</template>
+
+<script>
+  import TestComponent from '~/components/TestComponent.vue'
+  import AppLogo from '~/components/AppLogo.vue'
+  import AppButton from '../atom/AppButton.vue'
+  import RxTest from '~/components/RxTest.vue'
+  // Ajax通信ライブラリ
+  import axios from 'axios';
+
+  export default {
+    data: () => ({
+      loading: false,
+      content: "content text",
+      count: 0
+    }),
+
+    components: {
+      TestComponent,
+      AppLogo,
+      RxTest,
+    },
+
+    methods: {
+      fetch: function(){
+        this.loading = true;
+
+        axios
+          .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+          .then(response => {
+            this.content = response;
+            this.loading = false;
+          })
+      }
+    }
+  }
+</script>
