570de9afd8e412f7068463d5a55bfbd575460006
makoto-yoshida <makoto.yoshida@dena.com>
2018-08-17 21:08:25 +0900

    動画リストを追加したんご

--- repository/VideoRepository.ts
+++ repository/VideoRepository.ts
@@ -2,11 +2,13 @@ import Video from '../entity/Video'
 
 export default class VideoRepository {
   public static fetchVideoList(): [] {
-    //return await axios.get('https://hoge')
     const response = "{\n" +
       "\"src_list\": [" +
       "{\"src\": \"https://www.youtube.com/embed/srH34Tjjo9U\"}," +
-      "{\"src\": \"https://www.youtube.com/embed/paXpvS23N5w\"}" +
+      "{\"src\": \"https://www.youtube.com/embed/paXpvS23N5w\"}," +
+      "{\"src\": \"https://www.youtube.com/embed/4Ohpj2GDbRQ\"}," +
+      "{\"src\": \"https://www.youtube.com/embed/0dIs_vSG400\"}," +
+      "{\"src\": \"https://www.youtube.com/embed/dHm6BjJqIsE\"}" +
       "]" +
       "}"
     let videoObj = JSON.parse(response)
